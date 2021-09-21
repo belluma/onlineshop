7a1d5b51fbc5c276c04f5c91d743452f9ae736af
ben <benedikt.maciejczyk@gmx.de>
2021-09-21 20:42:43 +0200

    initialize empty project

---
+++ src/main/java/demo/onlineshop/OnlineshopApplication.java
@@ -0,0 +1,13 @@
+package demo.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshopApplication.class, args);
+    }
+
+}
